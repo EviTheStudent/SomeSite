b01bb0eade56aa5a968e1dde4fce78be427792b2
“EviTheStudent“ <evi.coventry@student.nhlstenden.com>
2025-12-10 12:02:31 +0100

    Making randomizer

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import *
+from math import Random
 
 # Configure application
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Basic Flask App"
+def SelectRand():
+    str imageNumber = Random(1, 37)
+    return send_file(f"Imgs/{imageNumber}.png", mimetype='image/jpg')
 
 @app.route("/about")
 def about():
- return send_file("Imgs30.png", mimetype='image/jpg')
+    return send_file("Imgs/30.png", mimetype='image/jpg')
 
 if __name__ == '__main__':
     app.run()
